f9c68942aaa6d74eb972efb672e8d009b87d3892
Quentin Dufour <quentin@dufour.tk>
2016-12-14 14:07:49 +0100

    WIP inject content

--- static/main.js
+++ static/main.js
@@ -1,3 +1,44 @@
+var content = [
+  {
+    article: {
+      title: "Bowie",
+      date: "16/02",
+      text: "RT si t'es triste",
+      video: {
+        webm: "/video/David_Bowie_Heroes.mkv.webm",
+        mp4: "/video/David_Bowie_Heroes.mkv.mp4",
+        png: "/video/David_Bowie_Heroes.mkv.png",
+      }
+    },
+    question: {
+      ask: "Nombre d'enfants",
+      answers: [
+        { text: "3", win: true},
+        { text: "26", win: false}
+      ]
+    }
+  },
+  {
+    article: {
+      title: "Loi travail",
+      date: "16/02",
+      text: "Travailler plus pour gagner plus",
+      video: {
+        webm: "/video/polina.webm",
+        mp4: "/video/polina.mp4",
+        png: "/video/David_Bowie_Heroes.mkv.png",
+      }
+    },
+    question: {
+      ask: "Nombre d'enfants",
+      answers: [
+        { text: "3", win: true},
+        { text: "26", win: false}
+      ]
+    }
+  },
+];
+
 var app = angular.module('jesuis', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -13,6 +54,13 @@ var app = angular.module('jesuis', ['ngRoute'])
         redirectTo: '/init'
       })
   }])
+
+  .filter("trustUrl", ['$sce', function ($sce) {
+    return function (recordingUrl) {
+      return $sce.trustAsResourceUrl(recordingUrl);
+    };
+  }])
+
  .controller('InitController', ['$scope', '$location', function($scope, $location) {
    console.log("init-controller");
    $scope.start = function() {
@@ -20,8 +68,16 @@ var app = angular.module('jesuis', ['ngRoute'])
      $location.path('/event');
    };
  }])
+
  .controller('EventController', ['$scope', '$location', function($scope, $location) {
    console.log("event-controller");
+   $scope.selected_content = 0;
+   $scope.content = content;
+   $scope.next = function() {
+     $scope.selected_content += 1;
+     $scope.load();
+     console.log("coucou");
+   };
  }])
 ;
 
