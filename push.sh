37ba0f5f2d59d9f620ba775f703ab08be09061f8
Quentin Dufour <quentin@dufour.tk>
2016-12-14 10:54:51 +0100

    Update Ouest Eclair -> France

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+ssh quentin@havelock.machine.deuxfleurs.fr -p110 rm -r /store/user/quentin/web/default/hyblab
 scp -r -P110 ./static quentin@havelock.machine.deuxfleurs.fr:~/web/default/hyblab
