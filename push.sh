f6b7a6e714e749161a9d1e2890bf3ed2facfa130
Quentin Dufour <quentin@dufour.tk>
2016-12-15 11:31:12 +0100

    I DISCLOZ MY PERSONAL INFORMATION

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rsync -ravz -e "ssh -p110" --progress ./static/* root@lupine.machine.deuxfleurs.fr:/var/www/html
 rsync -ravz -e "ssh -p110" --progress ./static/* quentin@havelock.machine.deuxfleurs.fr:~/web/default/hyblab
